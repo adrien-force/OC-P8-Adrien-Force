56da63c40eca0aeb10129641cb8bc4575e589a1c
Adrien <adrienforce@icloud.com>
2024-07-12 12:19:31 +0200

    bugged state : CSRF token

---
+++ src/Form/ProjectAddType.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Employe;
+use App\Entity\Project;
+use App\Entity\Status;
+use App\Entity\Tag;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProjectAddType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('startAt', null, [
+                'widget' => 'single_text'
+            ])
+            ->add('deadline', null, [
+                'widget' => 'single_text'
+            ])
+            //Archived field is not needed in the add form
+//            ->add('archived',
+//                CheckboxType::class,
+//                [
+//                    'label' => 'Archive',
+//                    'required' => false,
+//                ])
+            ->add('status', EntityType::class, [
+                'class' => Status::class,
+                'choice_label' => 'name',
+            ])
+            ->add('tags', EntityType::class, [
+                'class' => Tag::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+            ])
+            ->add('employes', EntityType::class, [
+                'class' => Employe::class,
+                'choice_label' => 'fullname',
+                'multiple' => true,
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Project::class,
+        ]);
+    }
+}
